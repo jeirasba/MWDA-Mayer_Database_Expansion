5ff7289df9cea9a98cb4d64077e984f77be6f3ec
jeirasba <61029011+jeirasba@users.noreply.github.com>
2026-05-07 20:04:20 +0200

    Delete Tracers_advection_a directory

--- Tracers_advection_a/mainfunctions/Launcher_2.sh
+++
@@ -1,7 +0,0 @@
-
-# vertical
-/home/jorge/miniconda3/envs/mwda307/bin/python main_ERA5_lamda.py --starting_date 2020-08-29 --starting_hour 0 --ending_date 2020-08-29 --ending_hour 18 --tracer vertical_theta --lamda_reciprocal_days 3 --savedir ./test_vertical_theta/
-# seasonality
-/home/jorge/miniconda3/envs/mwda307/bin/python main_ERA5_lamda.py --starting_date 2020-08-29 --starting_hour 0 --ending_date 2020-08-29 --ending_hour 18 --tracer seasonality_theta --lamda_reciprocal_days 3 --savedir ./test_seasonality_theta/
-# diabatic (variante theta)
-/home/jorge/miniconda3/envs/mwda307/bin/python main_ERA5_lamda.py --starting_date 2020-08-29 --starting_hour 0 --ending_date 2020-08-29 --ending_hour 18 --tracer diabatic_theta --lamda_reciprocal_days 3 --savedir ./test_diabatic_theta/
